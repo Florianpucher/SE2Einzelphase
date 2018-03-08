652b609ac66d4f7dc1d3bc2df45d8406884d30b1
Florian Pucher <florianpucher1@web.de>
2018-03-08 13:45:04 +0100

    Funktionalität implementiert

--- app/src/main/java/com/example/florian/palindrom/MainActivity.java
+++ app/src/main/java/com/example/florian/palindrom/MainActivity.java
@@ -8,8 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText input;
+    Button check;
+    String res;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        result = (TextView) findViewById(R.id.resultField);
+        input = (EditText) findViewById(R.id.eingabeString);
+
+        check = (Button) findViewById(R.id.checkButton);    //Button mit dem Code-Button verbinden
+        check.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String inputString = input.getText().toString();  //Input aus dem Feld zuweisen
+
+                if (inputString.isEmpty()){
+                    res = "Input ist leer";
+                    result.setText(res);
+                }
+                if (inputString.length() > 0 && inputString.length() < 5){
+                    res = "Input String zu kurz";
+                }
+                else{
+                    Boolean resB = isPalindrome(inputString);
+                    if (resB){
+                        res = "JA!";
+                    }
+                    else{
+                        res = "NEIN!";
+                    }
+                }
+                result.setText(res);
             }
         });
     }
@@ -48,5 +86,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static boolean isPalindrome(String str){
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
